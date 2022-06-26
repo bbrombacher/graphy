869d9d6e5bd677e99faf35870461b76595280843
bbrombacher <bbrombacher@me.com>
2022-06-26 16:32:43 -0700

    gauge

--- charts.js
+++ charts.js
@@ -60,6 +60,7 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     let samples = data.samples
+
     //console.log(samples)
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     samples = samples.filter(function(item){
@@ -121,5 +122,46 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    let metadata = data.metadata.filter((item) => sample == item.id) 
+
+    // 2. Create a variable that holds the first sample in the metadata array.
+    let firstMetadata = metadata[0]
+    console.log(firstMetadata)
+
+    // 3. Create a variable that holds the washing frequency.
+    let wfreq = parseFloat(firstMetadata.wfreq)
+
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { text: "<b>Belly Button Washing Frequency</b> <br><span style='font-size:0.8em;'>Scrubs per Week</span><br>"},
+        value: wfreq,
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { range: [null, 10]},
+          bar: { color: "black" },
+          steps: [
+            {range: [0, 2], color: "red"},
+            {range: [2, 4], color: "orange"},
+            {range: [4, 6], color: "yellow"},
+            {range: [6, 8], color: "limegreen"},
+            {range: [8, 10], color: "green"}
+          ]
+        }
+      }
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 500,
+      height: 500,
+    };
+    
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot(gauge, gaugeData, gaugeLayout);
   });
 }
