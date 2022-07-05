9a6b758e2dc805b355efc7ee8652735699fdd0df
bbrombacher <bbrombacher@me.com>
2022-07-04 21:35:49 -0700

    Update charts.js

--- charts.js
+++ charts.js
@@ -61,7 +61,6 @@ function buildCharts(sample) {
     // 3. Create a variable that holds the samples array. 
     let samples = data.samples
 
-    //console.log(samples)
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     samples = samples.filter(function(item){
           return sample == item.id
@@ -82,12 +81,14 @@ function buildCharts(sample) {
 
     // 8. Create the trace for the bar chart. 
     limitedSampleValues = sample_values.slice(0, 10).reverse()
+    limitedOtuLables = otuLables.slice(0, 10).reverse()
+
     var barData = [
       {
         x: limitedSampleValues,
         y: yticks,
         type: "bar",
-        text: otuLables,
+        text: limitedOtuLables,
         orientation: "h"
       }      
     ];
@@ -128,7 +129,6 @@ function buildCharts(sample) {
 
     // 2. Create a variable that holds the first sample in the metadata array.
     let firstMetadata = metadata[0]
-    console.log(firstMetadata)
 
     // 3. Create a variable that holds the washing frequency.
     let wfreq = parseFloat(firstMetadata.wfreq)
@@ -137,12 +137,16 @@ function buildCharts(sample) {
     var gaugeData = [
       {
         domain: { x: [0, 1], y: [0, 1] },
-        title: { text: "<b>Belly Button Washing Frequency</b> <br><span style='font-size:0.8em;'>Scrubs per Week</span><br>"},
+        title: { text: "<b>Belly Button Washing Frequency</b> </br><span style='font-size:0.8em;'>Scrubs per Week</span><br>"},
         value: wfreq,
         type: "indicator",
         mode: "gauge+number",
         gauge: {
-          axis: { range: [null, 10]},
+          axis: { 
+            range: [null, 10],
+            tickmode: "array",
+            tickvals: [2, 4, 6, 8, 10],
+          },
           bar: { color: "black" },
           steps: [
             {range: [0, 2], color: "red"},
@@ -158,10 +162,10 @@ function buildCharts(sample) {
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
       width: 450,
-      height: 450,
+      height: 450
     };
     
     // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot(gauge, gaugeData, gaugeLayout);
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
